b6f75d5be4293ea5810c879c55bced4322b25575
fff <ffff>
2014-01-07 19:55:34 +0800

    sssss

---
+++ about_us.php
@@ -0,0 +1,177 @@
+<?php	include_once '/var/www/icewoods/include/config.php';	?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<title>關於我 冰河森林數位科技有限公司</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Cache-Control" content="no-cache">
+<Meta http-equiv="Expires" content="-1">
+<meta http-equiv="imagetoolbar" content="false">
+<meta name="title" content="冰河森林數位科技有限公司" />
+<meta name="description" content="冰河森林數位科技有限公司 ICEWOODS DIGITAL TECHNOLOGY CO., Ltd www.icewoods.com"/>
+<meta name="COPYRIGHT" content="&amp; 2013 Icewoods. All Right Reserved." />
+<meta name="AUTHOR" content="Icewoods" />
+<link rel="image_src" href="image/icewoods_img.jpg" />
+<?php	include_once 'indexbody/default_css.php';	?>
+<?php	include_once 'indexbody/default_js.php';	?>
+<script src="js/about_us_self.js"></script>
+<link href="css/tw_about_us_style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<!--------首頁wapper開始--------->
+<div id="wapper">
+<!--------header語系切換及按讚區塊開始--------->
+<?php	include_once 'indexbody/header.php';	?>
+<!--------header語系切換及按讚區塊結束--------->
+<!----------------------------內容區塊開始-------------------------->
+<div id="content_all">
+<!-----------about_us上方主圖區開始---------->
+<div id="about_us_mainimage">
+    <div id="icewoods_logo_area"><?php	include_once 'indexbody/icewoods_logo.php';	?></div>
+</div>
+<!-----------about_us上方主圖區結束---------->
+<!-----------主選單開始--------------------->
+<?php	include_once 'indexbody/main_menu.php';	?>
+<!------------主選單結束-------------------->
+<!------------冰河故事開始------------------>
+<div id="icewoods_story">
+<!-------冰河標題開始-------->
+<div class="header_type_green" id="icewoods_story_header">冰河故事 <span class="header_type_gray">ICEWOODS STORY</span></div>
+<!-------冰河標題結束-------->
+<!-------冰河故事內容-------->
+<div id="icewoods_story_content">
+<div class="header_type_green2" id="icewoods_story_content_right">
+  <p>冰河小魚的故事...</p>
+  <p class="body_type_gray">冰河魚被惡魔詛咒，必須歷經破冰而出、順著冰河踏上回家艱難的道路，其間經歷朋友、伙伴的共同努力打氣，擊敗惡魔化解魔咒，最終抵達大海上的島嶼，創造天空之城的傳奇。
+</div>
+</p><div id="icewoods_story_movie"><iframe width="300" height="169" src="//www.youtube.com/embed/lvCdOhAaQcE" frameborder="0" allowfullscreen></iframe></div>
+</div>
+<!-------冰河故事內容-------->
+<!-----前往文創內容開始---->
+<div id="link_to_webpage"><a href="org_content.php">TO..文創內容</a></div>
+<!-----前往文創內容結束---->
+</div>
+<!------------冰河故事結束------------------>
+<!------------最新消息開始------------------>
+<?php	include_once 'indexbody/news_list.php';	?>
+<!------------最新消息結束------------------>
+<!------------關於我開始------------------>
+<div id="about_us_all">
+<!------標題開始-------->
+<div class="header_type_green" id="icewoods_story_header">關於我 <span class="header_type_gray">ABOUT US</span></div>
+<!-------標題結束-------->
+<!-------內文開始-------->
+<div class="body_type_gray" id="about_us_content">作為一間數位內容暨網際網路服務公司，我們有很大很大的夢想。公司成立於2007年，自我期許像是一顆破冰而出的小樹苗，不怕外在生存環境的困難、努力奮鬥，夢想有一天可以蔓延成一整片廣大無邊的冰河森林。我們對數位娛樂、文化創意充滿熱情，我們相信網際網路與創新的力量，透過我們穩健的線上商務發展，創造一個更棒的世界。
+</div>
+<!-------內文結束-------->
+<!-------內文圖示區-------->
+<div id="about_us_info_graphic">
+<div id="info_graphic_left"><img src="image/index_about_us_img2.png" width="430" height="430" /></div>
+<div id="info_graphic_right">
+<!-------圖示模組-------->
+<div id="service_module">
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td align="center"><img src="image/index_about_us_icon01.png" width="130" height="130" /></td>
+  </tr>
+  <tr>
+    <td class="body_type_gray">服務超過</td>
+  </tr>
+  <tr>
+    <td><span class="number_big">80</span><img src="image/index_about_us_icon05.png" width="40" height="40" /></td>
+  </tr>
+  <tr>
+    <td class="body_type_gray">個以上的國家和城市</td>
+  </tr>
+</table></div>
+<!-------圖示模組-------->
+<!-------圖示模組-------->
+<div id="service_module">
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td align="center"><img src="image/index_about_us_icon02.png" width="130" height="130" /></td>
+  </tr>
+  <tr>
+    <td class="body_type_gray">結合超過全球</td>
+  </tr>
+  <tr>
+    <td><span class="number_big">80</span><img src="image/index_about_us_icon05.png" width="40" height="40" /></td>
+  </tr>
+  <tr>
+    <td class="body_type_gray">個以上的網路平台。</td>
+  </tr>
+</table></div>
+<!-------圖示模組-------->
+<!-------圖示模組-------->
+<div id="service_module">
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td align="center"><img src="image/index_about_us_icon03.png" width="130" height="130" /></td>
+  </tr>
+  <tr>
+    <td class="body_type_gray">每年服務超過全球</td>
+  </tr>
+  <tr>
+    <td><span class="service_module_number">25</span><img src="image/index_about_us_icon05.png" width="40" height="40" /></td>
+  </tr>
+  <tr>
+    <td class="body_type_gray">萬人次以上的線上客戶。 </td>
+  </tr>
+</table></div>
+<!-------圖示模組-------->
+<!-------圖示模組-------->
+<div id="service_module">
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td align="center"><img src="image/index_about_us_icon04.png" width="130" height="130" /></td>
+  </tr>
+  <tr>
+    <td class="body_type_gray">目前線上銷售</td>
+  </tr>
+  <tr>
+    <td><span class="service_module_number">3000</span><img src="image/index_about_us_icon05.png" width="40" height="40" /></td>
+  </tr>
+  <tr>
+    <td class="body_type_gray">種各類型的商品。 </td>
+  </tr>
+</table></div>
+<!-------圖示模組-------->
+</div>
+</div>
+<!-------內文圖示區-------->
+<!-----企業認證開始---->
+<div id="license">
+<div id="license_img"><img src="image/license_dandb.png" width="104" height="104" /></div>
+<div id="license_type_area">
+  <p class="header_type_green2">鄧白氏企業認證 D&B D-U-N-S Registered ™</p>
+  <p class="body_type_gray">目前公司取得美國網際網路，公正的第三方機構的鄧白氏企業認證 (D&amp;B D-U-N-S Registered ™) ，讓合作的廠商確定我們在網際網路上的商業信用。鄧白氏認證主要是讓我們的客戶，確認我們的企業可信度與資訊透明度，共創更多的商機與合作機會。</p>
+</div>
+</div>
+<!-----企業認證結束---->
+<!-------全部消息-------->
+<div id="link_to_webpage"><a href="b2b_form.php" target="_NEW">B2B 商務合作</a></div>
+<!-------全部消息-------->
+</div>
+<!------------關於我結束------------------>
+<!------------合作廠商logo開始------------------>
+<div id="our_client">
+<!------標題開始-------->
+<div class="header_type_green" id="icewoods_story_header">合作廠商 <span class="header_type_gray">CLIENT &amp; COOPERATION</span></div>
+<!-------標題結束-------->
+<!-------合作廠商logo-------->
+<div id="our_client_logo"><img src="image/client_logo.png" width="880" height="740" /></div>
+<!-------合作廠商logo-------->
+<!-------全部消息--------><!-------全部消息-------->
+</div>
+<!------------合作廠商logo結束------------------>
+</div>
+<!----------------------------內容區塊結束-------------------------->
+<!--------footer區塊開始--------------------->
+<?php	include_once 'indexbody/footer.php';	?>
+<!--------footer區塊結束--------------------->
+</div>
+<!--------首頁wapper結束--------->
+</body>
+</html>
