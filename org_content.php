cd652f09509325ffc98474ec8cfae97b2e227036
fff <ffff>
2014-01-07 20:14:01 +0800

    ssss

---
+++ org_content.php
@@ -0,0 +1,187 @@
+<?php	include_once '/var/www/icewoods/include/config.php';	?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<title>文創內容 冰河森林數位科技有限公司</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Cache-Control" content="no-cache">
+<Meta http-equiv="Expires" content="-1">
+<meta http-equiv="imagetoolbar" content="false">
+<meta name="title" content="冰河森林數位科技有限公司" />
+<meta name="description" content="冰河森林數位科技有限公司 ICEWOODS DIGITAL TECHNOLOGY CO., Ltd www.icewoods.com"/>
+<meta name="COPYRIGHT" content="&amp; 2013 Icewoods. All Right Reserved." />
+<meta name="AUTHOR" content="Icewoods" />
+<link rel="image_src" href="image/icewoods_img.jpg" />
+<?php	include_once 'indexbody/default_css.php';	?>
+<?php	include_once 'indexbody/default_js.php';	?>
+<script src="js/org_content_self.js"></script>
+<link href="css/tw_org_content_style.css" rel="stylesheet" type="text/css" /><link href="font_style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="OverDiv" align="center"></div>
+<!--------首頁wapper開始--------->
+<div id="wapper">
+<!--------header語系切換及按讚區塊開始--------->
+<?php	include_once 'indexbody/header.php';	?>
+<!--------header語系切換及按讚區塊結束--------->
+<!----------------------------內容區塊開始-------------------------->
+<div id="content_all">
+<!-----------about_us上方主圖區開始---------->
+<div id="about_us_mainimage">
+    <div id="icewoods_logo_area"><?php	include_once 'indexbody/icewoods_logo.php';	?></div>
+</div>
+<!-----------about_us上方主圖區結束---------->
+<!-----------主選單開始--------------------->
+<?php	include_once 'indexbody/main_menu.php';	?>
+<!------------主選單結束-------------------->
+<!------------原創內容開始------------------>
+<div id="org_content_all">
+<!-------平台概念標題開始-------->
+<div class="header_type_green" id="org_content_header">原創內容 <span class="header_type_gray">ORIGINAL CONTENT</span></div>
+<!-------平台概念標題結束-------->
+<!-------內文開始-------->
+<div class="body_type_gray" id="org_content_type">文創內容的開發是我們認為不論是在現實社會或網路世界中，最能感動人心、創造認同的傳達方式，尤其是網路的虛擬化世界，透過網路我們可以更天馬行空的發揮想像力，創造不一樣的商業價值。下面是我們自製的冰河魚企業動畫短片，歡迎欣賞。<br />
+</div>
+<!-------內文結束-------->
+<!-------冰河魚影片-------->
+<div id="iwoo_movie"><iframe width="800" height="450" src="//www.youtube.com/embed/lvCdOhAaQcE" frameborder="0" allowfullscreen></iframe></div>
+<!-------冰河魚影片-------->
+</div>
+<!------------原創內容結束------------------>
+<!------------設計理念開始------------------>
+<div id="design_mind_all">
+<!-------設計理念標題開始-------->
+<div class="header_type_green" id="org_content_header">設計理念 <span class="header_type_gray">DESIGN MIND</span></div>
+<!-------設計理念標題結束-------->
+<!-------設計理念內容開始-------->
+<div id="design_mind_content">
+<div class="header_type_green2" id="design_mind_right">
+  <p>品牌+美術設計團隊   Art+Brand Vision Division</p>
+  <p class="body_type_gray">我們的設計團隊專注於結合文化與創意，透過品牌規畫、動畫開發、影片製作、遊戲設計、產品設計、圖像授權、參與文創活動，來創造品牌價值。未來我們會持續專注在文創與商業的深度結合，以創造國際化品牌為目標，開創台灣文化的國際視野。</p>
+  <p class="body_type_gray">&nbsp;</p>
+  <p>品牌授權業務 Brand cooperation</p>
+  <p class="body_type_gray">我們自行開發了很多圖象來提升我們的品牌價值，但最終我們希望讓更多人感動，然而我們的力量有限，如果你喜歡、也認同我們的做法，歡迎您與我們連繫。</p>
+</div>
+</div>
+<!-------設計理念內容結束-------->
+<!-----前往連絡我們開始---->
+<div id="link_to_webpage"><a href="contact_us.php">TO..聯絡我們</a></div>
+<!-----前往連絡我們結束---->
+</div>
+<!------------設計理念結束------------------>
+<!------------動畫的力量開始------------------>
+<div id="ani_and_movie_all">
+<!-------設計理念標題開始-------->
+<div class="header_type_green" id="org_content_header">動畫的力量 <span class="header_type_gray">ANIMATION &amp; MOVIE</span></div>
+<!-------設計理念標題結束-------->
+<!-------內文開始-------->
+<div class="body_type_gray" id="org_content_type">藉由各式各樣不同的動畫創作，開發或紀錄不同的商業專案，透過網路的渲染力，快速的將台灣文創的力量感動全世界。目前我們在短短1年的時間內，完成超過50個不同的影片專案，累積超過150分鐘的影片時數。<br />
+</div>
+<!-------內文結束-------->
+<!-------影片展示開始-------->
+<div id="all_movie">
+<!-------影片-------->
+<div id="movie_module"><iframe width="380" height="214" src="//www.youtube.com/embed/8uZEh6MY65I?list=UUCpF_FHm8FzAB367toTrJeg" frameborder="0" allowfullscreen></iframe></div>
+<!-------影片-------->
+<!-------影片-------->
+<div id="movie_module"><iframe width="380" height="214" src="//www.youtube.com/embed/mqi9yiNpc5I" frameborder="0" allowfullscreen></iframe></div>
+<!-------影片-------->
+<!-------影片-------->
+<div id="movie_module"><iframe width="285" height="214" src="//www.youtube.com/embed/ojmQbVW7wJk" frameborder="0" allowfullscreen></iframe></div>
+<!-------影片-------->
+<!-------影片-------->
+<div id="movie_module"><iframe width="380" height="214" src="//www.youtube.com/embed/xPC_3E28kgs?list=UUCpF_FHm8FzAB367toTrJeg" frameborder="0" allowfullscreen></iframe></div>
+<!-------影片-------->
+</div>
+<!-------影片展示結束-------->
+<!-------內文結束-------->
+<!-----前往連絡我們開始---->
+<div id="link_to_webpage"><a href="https://www.youtube.com/user/icewoods168" target="_NEW">TO..更多影片</a></div>
+<!-----前往連絡我們結束---->
+</div>
+<!------------動畫的力量結束------------------>
+<!------------創意相本開始-------------------->
+<div id="creative_album_all">
+<!---標題開始---->
+<div class="header_type_green" id="org_content_header">創意相本 <span class="header_type_gray">CREATIVE PHOTO ALBUM</span></div>
+<!---標題結束---->
+<!---內文開始---->
+<div class="body_type_gray" id="org_content_type">這邊紀錄下一些我們從事的有趣活動，多半都會與創意結合，所以整理分享給大家，希望大家也會感到新奇有趣。
+  <br />
+</div>
+<!---內文結束---->
+<!-------照片區-------->
+<?php	include_once 'indexbody/creative_album.php';	?>
+<!-------照片區-------->
+</div>
+<!------------創意相本結束-------------------->
+<!------------桌布下載開始-------------------->
+<div id="download_wallpaper_all">
+<!---標題開始---->
+<div class="header_type_green" id="org_content_header">桌布下載 <span class="header_type_gray">DOWNLOAD WALLPAPER</span></div>
+<!---標題結束---->
+<!---內文開始---->
+<div class="body_type_gray" id="org_content_type">這裡提供了一些有趣的桌布，您可以用平板或手機、電腦，下載您喜歡的桌布。 <br />
+</div>
+<!---內文結束---->
+<!---桌布下載區開始---->
+<div id="wallpaper_all">
+<!---桌布左邊---->
+<div id="wallpaper_left">
+  <ul>
+    <li class="iphone"><a href="wallpaper/iw_wallpaper_05_phone.jpg" target="new">IPHONE</a></li>
+    <li class="ipad"><a href="wallpaper/iw_wallpaper_05_pad.jpg" target="new">IPAD</a></li>
+    <li class="computer"><a href="wallpaper/iw_wallpaper_05_pc.jpg" target="new">COMPUTER</a></li>
+  </ul>
+</div>
+<!---桌布左邊---->
+<!---桌布右邊---->
+<div id="wallpaper_right">
+<div id="wallpaper_right_img"><img src="image/wapaper_img_02.png" width="240" height="180" /></div>
+ <ul>
+    <li class="iphone"><a href="wallpaper/iw_wallpaper_01_phone.jpg" target="new">IPHONE</a></li>
+    <li class="ipad"><a href="wallpaper/iw_wallpaper_01_pad.jpg" target="new">IPAD</a></li>
+    <li class="computer"><a href="wallpaper/iw_wallpaper_01_pc.jpg" target="new">COMPUTER</a></li>
+  </ul></div>
+<!---桌布右邊---->
+<!---桌布右邊---->
+<div id="wallpaper_right">
+<div id="wallpaper_right_img"><img src="image/wapaper_img_03.png" width="240" height="180" /></div>
+ <ul>
+    <li class="iphone"><a href="wallpaper/iw_wallpaper_02_phone.jpg" target="new">IPHONE</a></li>
+    <li class="ipad"><a href="wallpaper/iw_wallpaper_02_pad.jpg" target="new">IPAD</a></li>
+    <li class="computer"><a href="wallpaper/iw_wallpaper_02_pc.jpg" target="new">COMPUTER</a></li>
+  </ul></div>
+<!---桌布右邊---->
+<!---桌布右邊---->
+<div id="wallpaper_right">
+<div id="wallpaper_right_img"><img src="image/wapaper_img_04.png" width="240" height="180" /></div>
+ <ul>
+    <li class="iphone"><a href="wallpaper/iw_wallpaper_03_phone.jpg" target="new">IPHONE</a></li>
+    <li class="ipad"><a href="wallpaper/iw_wallpaper_03_pad.jpg" target="new">IPAD</a></li>
+    <li class="computer"><a href="wallpaper/iw_wallpaper_03_pc.jpg" target="new">COMPUTER</a></li>
+  </ul></div>
+<!---桌布右邊---->
+<!---桌布右邊---->
+<div id="wallpaper_right">
+<div id="wallpaper_right_img"><img src="image/wapaper_img_05.png" width="240" height="180" /></div>
+ <ul>
+    <li class="iphone"><a href="wallpaper/iw_wallpaper_04_phone.jpg" target="new">IPHONE</a></li>
+    <li class="ipad"><a href="wallpaper/iw_wallpaper_04_pad.jpg" target="new">IPAD</a></li>
+    <li class="computer"><a href="wallpaper/iw_wallpaper_04_pc.jpg" target="new">COMPUTER</a></li>
+  </ul></div>
+<!---桌布右邊---->
+</div>
+<!----桌布下載結束----->
+</div>
+<!------------桌布下載結束-------------------->
+</div>
+<!----------------------------內容區塊結束-------------------------->
+<!--------footer區塊開始--------------------->
+<?php	include_once 'indexbody/footer.php';	?>
+<!--------footer區塊結束--------------------->
+</div>
+<!--------首頁wapper結束--------->
+</body>
+</html>
